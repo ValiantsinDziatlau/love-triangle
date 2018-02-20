1e3fef235fd85c4069f45cc89da7b671d53b5270
ValiantsinDziatlau <valiantsindziatlau@gmail.com>
2018-02-20 22:50:09 +0300

    NoSpeakingVariablesMk

--- src/index.js
+++ src/index.js
@@ -3,5 +3,13 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+    let count=0;
+    for(var i=0;i<preferences.length;i++){
+        if(preferences[preferences[preferences[i]-1]-1]==(i+1)&&preferences[i]!=preferences[preferences[i]-1]&&preferences[i]-1>=0){
+            //only triangles, no self-lovers, only positive
+            count++;
+                }
+    }
+    count/=3;
+  return count;
+  };
